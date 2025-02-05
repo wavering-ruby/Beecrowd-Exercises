ea8e7bba108075988f8dd96aab7f745e8b5f5f97
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-02-04 23:10:26 -0300

    Create 2006.java

---
+++ Java/2006.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+        
+        int t = s.nextInt();
+        int cont = 0;
+        int temp = 0;
+        
+        s.nextLine();
+        
+        String v = s.nextLine();
+        
+        // System.out.print(v.length());
+        
+        s.close();
+        
+        for(int i = 0; i < v.length(); i++){
+            char c = v.charAt(i);
+            
+            if(c != ' '){
+                
+                // Tem duas maneiras para converter essa lógica, utilizando o - '0' ou transformar para String
+                // A lógica do - '0' é porque estariamos subtraindo daquele valor 0 na tabela ASCII, já que o zero significaria estarmos estraíndo 48 do código da tabela ASCII daquele número e convertendo para inteiro. Por exemplo, '1' na tabela ASCII é o equivalente a 49, subtraindo 48 dessa função ele vira 1 inteiro e não mais 49.
+                temp = c - '0';
+                
+                // Também temos a lógica de converter de char para string e depois para int
+                // temp = Integer.parseInt(String.valueOf(c));
+                
+                if(temp == t){
+                    cont++;
+                }
+            }
+        }
+        
+        System.out.println(cont);
+    
+    }
+}
