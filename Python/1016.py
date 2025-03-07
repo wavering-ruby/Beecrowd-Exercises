dec4fa7e2b8b6eec9b0139a1848ab657a1ca0968
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-03-07 14:10:01 -0300

    New exercise

---
+++ Python/1016.py
@@ -0,0 +1,9 @@
+d = int(input());
+
+# O exercício pede para calcular em quantos minutos o carro Y demoraria
+# para estar na frente do carro X, com base em uma distância dada.
+
+# Em geral, o exercício fala que basicamente o carro Y a cada 1km de distância,
+# ele demora cerca de 2 minutos. 
+
+print(f"{d * 2} minutos");
