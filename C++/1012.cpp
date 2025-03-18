e7882b9490a090b5012872473ec40816a3064079
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-03-18 12:47:14 -0300

    Simplified the code <Verified>

--- C++/1012.cpp
+++ C++/1012.cpp
@@ -6,10 +6,12 @@ int main(){
     pi = 3.14159;
     std::cin >> a >> b >> c;
     
-    std::cout << std::fixed << "TRIANGULO: " << std::setprecision(3) << (a * c) / 2 << std::endl;
-    std::cout << std::fixed << "CIRCULO: " << std::setprecision(3) <<  (c * c) * pi << std::endl;
-    std::cout << std::fixed << "TRAPEZIO: " << std::setprecision(3) << ((a + b) * c) / 2 << std::endl;
-    std::cout << std::fixed << "QUADRADO: " << std::setprecision(3) << (b * b) << std::endl;
-    std::cout << std::fixed << "RETANGULO: " << std::setprecision(3) << (a * b) << std::endl;
+    std::cout << std::fixed << std::setprecision(3);
+
+    std::cout << "TRIANGULO: " << (a * c) / 2 << std::endl;
+    std::cout << "CIRCULO: " <<  (c * c) * pi << std::endl;
+    std::cout << "TRAPEZIO: " << ((a + b) * c) / 2 << std::endl;
+    std::cout << "QUADRADO: " << (b * b) << std::endl;
+    std::cout << "RETANGULO: " << (a * b) << std::endl;
     return 0;
 }
