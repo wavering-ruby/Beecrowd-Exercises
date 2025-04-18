c0e808a9fc8d0d36ec8436e70d1dfb1f410e7da5
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-04-18 01:25:17 -0300

    Add files via upload

---
+++ C++/1. Beginner/1113.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    
+    while(true){
+        cin >> x >> y;
+        
+        if(x == y){
+            break;
+        }
+        
+        if(x > y){
+            cout << "Decrescente" << endl;
+        } else {
+            cout << "Crescente" << endl;
+        }
+    }
+
+    return 0;
+}
