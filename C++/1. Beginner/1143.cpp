382bc261c77f294507554a17974c6e6767c10daf
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-04-20 22:22:39 -0300

    Add files via upload

---
+++ C++/1. Beginner/1143.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++){ // Quantidade de linhas
+        
+        for(int j = 1; j <= 3; j++){
+            if(j != 3){
+                cout << pow(i, j) << " ";
+            } else {
+                cout << pow(i, j) << endl;
+            }
+        }
+    }
+
+    return 0;
+}
