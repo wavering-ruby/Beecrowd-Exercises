5547bb900d8686a94f4562d6e1a981ccf7a42617
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-04-30 09:52:51 -0300

    Updating the folders for this file

--- .Frontend/Relatory.py
+++ .Frontend/Relatory.py
@@ -53,7 +53,9 @@ def get_folder_structure(base_path, script_dir, extensions=None):
     if extensions is None:
         extensions = [".cpp"]  # Valor padrão se não for especificado
 
-    folders = ["1. Beginner", "2. Ad-Hoc", "4. Structures and Librarys"]
+    # Nessa variável, deve-se ter todos os nomes das pastas da maneira CORRETA que estão no diretório
+    # Se qualquer coisa estiver errada, ele retornará erro do sistema não encontrar o caminho especificado!
+    folders = ["1. Beginner", "2. Ad-Hoc", "3. Mathematics", "4. Structures and Librarys"]
     folder_data = []
     
     for folder in folders:
