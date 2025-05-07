d2a3a4551c7b64c455829634450286c2266decd8
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-05-07 00:12:03 -0300

    Add files via upload

---
+++ C++/2. Ad-Hoc/2413.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int link3, link1;
+    
+    cin >> link3;
+    
+    link1 = (link3 * 2) * 2;
+
+    cout << link1 << endl;
+
+    return 0;
+}
