438956e44c5b94bb13e6283f946a8344a87767a6
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-07-16 23:14:27 -0300

    New exercise

---
+++ C++/1. Beginner/1864.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string phrase = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    
+    int n; 
+    
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        cout << phrase[i];
+    }
+    
+    cout << endl;
+    return 0;
+}
