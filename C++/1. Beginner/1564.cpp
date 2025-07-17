c0c12f198aeb498f311bb3962aa2078652299a35
Mateus Gabriel <82602817+wavering-ruby@users.noreply.github.com>
2025-07-16 22:35:19 -0300

    New exercise

---
+++ C++/1. Beginner/1564.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    
+    while(cin >> n){
+        if(n <= 0){
+            cout << "vai ter copa!" << endl;
+        } else {
+            cout << "vai ter duas!" << endl;
+        }
+    }
+
+    return 0;
+}
